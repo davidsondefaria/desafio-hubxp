d41bb6efe8960352cd6f57dd1ede94467ed27803
Davidson <davidsondefaria@gmail.com>
2023-10-19 15:12:54 -0300

    feat: storing time between clicks

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let pattern = [];
+let lastClick = null;
+
 document.addEventListener("DOMContentLoaded", () => {
   const divElement = document.getElementById("clickableDiv");
 
@@ -7,5 +10,20 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function handleClick(event) {
-  console.log("Div clicked!");
+  const currentTime = new Date().getTime();
+
+  handleTime(currentTime);
+
+  pattern.push("click");
+
+  console.log("Div clicked!", pattern);
+}
+
+function handleTime(currentTime) {
+  if (lastClick !== null) {
+    const timeElapsed = currentTime - lastClick;
+    pattern.push(timeElapsed);
+  }
+
+  lastClick = currentTime;
 }
